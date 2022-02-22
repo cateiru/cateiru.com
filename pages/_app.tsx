c724d415318b4b8359b8c91961fb79a9bb50aa66
Yuto Watanabe <yuto.w51942@gmail.com>
2022-02-22 19:26:32 +0900

    initialize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {ChakraProvider} from '@chakra-ui/react';
+import type {AppProps} from 'next/app';
+import Router, {useRouter} from 'next/router';
+import nprogress from 'nprogress';
+import {useEffect} from 'react';
+import {RecoilRoot} from 'recoil';
+import {GA_TRACKING_ID, pageview} from '../utils/ga/gtag';
+import theme from '../utils/theme/theme';
+import 'nprogress/nprogress.css';
+import Font from '../components/common/Font'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+nprogress.configure({showSpinner: false, speed: 400, minimum: 0.25});
 
-export default MyApp
+const App = ({Component, pageProps}: AppProps) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  Router.events.on('routeChangeStart', () => {
+    nprogress.start();
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    nprogress.done();
+  });
+
+  Router.events.on('routeChangeError', () => {
+    nprogress.done();
+  });
+
+  return (
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+          <Font />
+              <Component {...pageProps} />
+      </ChakraProvider>
+    </RecoilRoot>
+  );
+};
+
+export default App;
---
+++ utils/theme/theme.ts
@@ -0,0 +1,36 @@
+import {extendTheme} from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: "'Noto Sans JP', sans-serif",
+    body: "'Noto Sans JP', sans-serif",
+  },
+  components: {
+    CloseButton: {
+      baseStyle: {
+        _focus: {
+          boxShadow: 'none',
+        },
+      },
+    },
+  },
+  styles: {
+    global: (props: {colorMode: string}) => ({
+      // Chrome
+      '&::-webkit-scrollbar': {
+        width: '10px',
+      },
+      '&::-webkit-scrollbar-thumb': {
+        backgroundColor: props.colorMode === 'dark' ? 'gray.600' : 'gray.300',
+        borderRadius: '100px',
+      },
+      // FireFox
+      html: {
+        scrollbarWidth: 'thin',
+        scrollbarColor: props.colorMode === 'dark' ? 'gray.600' : 'gray.300',
+      },
+    }),
+  },
+});
+
+export default theme;
