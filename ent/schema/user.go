b0e77a9013a1ded146b926daa9221de979681722
Yuto Watanabe <yuto.w51942@gmail.com>
2022-09-20 17:04:44 +0900

    generate ent

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -1,7 +1,11 @@
 package schema
 
 import (
+	"time"
+
 	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/dialect/entsql"
 	"entgo.io/ent/schema/field"
 )
 
@@ -13,7 +17,82 @@ type User struct {
 // Fields of the User.
 func (User) Fields() []ent.Field {
 	return []ent.Field{
-		field.Uint("id")
+		// `id` INT UNSIGNED AUTO_INCREMENT NOT NULL
+		field.Uint32("id"),
+
+		// `given_name` VARCHAR(10) NOT NULL
+		field.String("given_name").
+			Annotations(entsql.Annotation{
+				Size: 10,
+			}),
+
+		// `family_name` VARCHAR(10) NOT NULL
+		field.String("family_name").
+			Annotations(entsql.Annotation{
+				Size: 10,
+			}),
+
+		// `given_name_ja` VARCHAR(10) NOT NULL
+		field.String("given_name_ja").
+			Annotations(entsql.Annotation{
+				Size: 10,
+			}),
+
+		// `family_name_ja` VARCHAR(10) NOT NULL
+		field.String("family_name_ja").
+			Annotations(entsql.Annotation{
+				Size: 10,
+			}),
+
+		// `user_id` VARCHAR(10) NOT NULL
+		field.String("user_id").
+			Annotations(entsql.Annotation{
+				Size: 10,
+			}),
+
+		// `mail` VARCHAR(256) NOT NULL
+		field.String("mail").
+			Annotations(entsql.Annotation{
+				Size: 254,
+			}),
+
+		// `birth_date` DATE NOT NULL
+		field.Time("birth_date").
+			SchemaType(map[string]string{
+				dialect.MySQL: "date",
+			}),
+
+		// `location` TEXT NOT NULL
+		field.Text("location").SchemaType(map[string]string{
+			dialect.MySQL: "text",
+		}),
+
+		// `location_ja` TEXT NOT NULL
+		field.Text("location_ja").SchemaType(map[string]string{
+			dialect.MySQL: "text",
+		}),
+
+		// `created` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+		field.Time("created").
+			SchemaType(map[string]string{
+				dialect.MySQL: "datetime",
+			}).
+			Default(time.Now).
+			Annotations(entsql.Annotation{
+				Default: "CURRENT_TIMESTAMP",
+			}),
+
+		// `modified` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+		field.Time("modified").
+			SchemaType(map[string]string{
+				dialect.MySQL: "datetime",
+			}).
+			Default(time.Now).
+			UpdateDefault(time.Now).
+			Annotations(entsql.Annotation{
+				// FIXME: If DDL is available, use it. ref.https://github.com/ent/ent/issues/558
+				Default: "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+			}),
 	}
 }
 
