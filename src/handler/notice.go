f6190598997026e9ac5d8b7e2d1ed8813a67e155
Yuto Watanabe <yuto.w51942@gmail.com>
2022-10-30 16:19:04 +0900

    added notice handler

--- src/handler/notice.go
+++ src/handler/notice.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"context"
 	"net/http"
-	"strconv"
 
 	"github.com/cateiru/cateiru.com/ent/notice"
 	"github.com/labstack/echo/v4"
@@ -27,42 +26,6 @@ func (h *Handler) NoticeHandler(e echo.Context) error {
 	return e.JSON(http.StatusOK, n)
 }
 
-func (h *Handler) CreateNoticeHandler(e echo.Context) error {
-	ctx := context.Background()
-
-	if err := h.Session(ctx, e); err != nil {
-		return err
-	}
-
-	n := h.DB.Client.Notice.
-		Create().
-		SetUserID(h.User.ID)
-
-	discordWebhook := e.FormValue("discord_webhook")
-	if discordWebhook != "" {
-		n = n.SetDiscordWebhook(discordWebhook)
-	}
-	slackWebhook := e.FormValue("slack_webhook")
-	if slackWebhook != "" {
-		n = n.SetDiscordWebhook(slackWebhook)
-	}
-	mail := e.FormValue("mail")
-	if mail != "" {
-		if err := ValidateMail(mail); err != nil {
-			return err
-		}
-
-		n = n.SetMail(mail)
-	}
-
-	noticeD, err := n.Save(ctx)
-	if err != nil {
-		return err
-	}
-
-	return e.JSON(http.StatusCreated, noticeD)
-}
-
 func (h *Handler) UpdateNoticeHandler(e echo.Context) error {
 	ctx := context.Background()
 
@@ -70,25 +33,25 @@ func (h *Handler) UpdateNoticeHandler(e echo.Context) error {
 		return err
 	}
 
-	idStr := e.FormValue("notice_id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "invalid form: notice_id")
-	}
-
 	n := h.DB.Client.Notice.
 		Update().
-		Where(notice.ID(uint32(id)))
+		Where(notice.UserID(h.User.ID))
 
 	discordWebhook := e.FormValue("discord_webhook")
 	if discordWebhook != "" {
+		if err := ValidateURL(discordWebhook); err != nil {
+			return err
+		}
 		n = n.SetDiscordWebhook(discordWebhook)
 	} else {
 		n = n.ClearDiscordWebhook()
 	}
 	slackWebhook := e.FormValue("slack_webhook")
 	if slackWebhook != "" {
-		n = n.SetDiscordWebhook(slackWebhook)
+		if err := ValidateURL(slackWebhook); err != nil {
+			return err
+		}
+		n = n.SetSlackWebhook(slackWebhook)
 	} else {
 		n = n.ClearSlackWebhook()
 	}
@@ -109,28 +72,4 @@ func (h *Handler) UpdateNoticeHandler(e echo.Context) error {
 	}
 
 	return e.JSON(http.StatusCreated, noticeD)
-}
-
-func (h *Handler) DeleteNoticeHandler(e echo.Context) error {
-	ctx := context.Background()
-
-	if err := h.Session(ctx, e); err != nil {
-		return err
-	}
-
-	idStr := e.QueryParam("notice_id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "invalid form: notice_id")
-	}
-
-	_, err = h.DB.Client.Notice.
-		Delete().
-		Where(notice.ID(uint32(id))).
-		Exec(ctx)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
--- src/handler/notice_test.go
+++ src/handler/notice_test.go
@@ -2,11 +2,14 @@ package handler_test
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/cateiru/cateiru.com/ent"
+	"github.com/cateiru/cateiru.com/ent/notice"
 	"github.com/cateiru/cateiru.com/src/handler"
 	"github.com/cateiru/cateiru.com/src/test"
+	"github.com/cateiru/go-http-easy-test/contents"
 	"github.com/cateiru/go-http-easy-test/handler/mock"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/require"
@@ -60,26 +63,104 @@ func TestNoticeHandler(t *testing.T) {
 	})
 }
 
-func TestCreateNoticeHandler(t *testing.T) {
-	test.Init()
-
-	test.LoginTestGet(t, func(h *handler.Handler, e echo.Context) error {
-		return h.CreateNoticeHandler(e)
-	})
-}
-
 func TestUpdateNoticeHandler(t *testing.T) {
 	test.Init()
 
+	t.Run("success", func(t *testing.T) {
+		ctx := context.Background()
+
+		tool, err := test.NewTestTool()
+		require.NoError(t, err)
+		defer tool.Close()
+
+		err = tool.ClearNotice(ctx)
+		require.NoError(t, err)
+
+		h, err := tool.Handler()
+		require.NoError(t, err)
+
+		u, err := tool.NewUser(ctx)
+		require.NoError(t, err)
+
+		n, err := u.CreateNotice()
+		require.NoError(t, err)
+		_, err = n.CreateDB(ctx, tool.DB)
+		require.NoError(t, err)
+
+		form := contents.NewMultipart()
+		form.Insert("discord_webhook", "https://webhook.discord.com/hogehoge")
+
+		m, err := mock.NewFormData("/", form, http.MethodPut)
+		require.NoError(t, err)
+
+		err = u.HandlerSession(ctx, tool.DB, m)
+		require.NoError(t, err)
+
+		e := m.Echo()
+
+		err = h.UpdateNoticeHandler(e)
+		require.NoError(t, err)
+
+		nDB, err := tool.DB.Client.Notice.
+			Query().
+			Where(notice.UserID(u.User.ID)).
+			First(ctx)
+		require.NoError(t, err)
+
+		require.Equal(t, nDB.Mail, "")
+		require.Equal(t, nDB.DiscordWebhook, "https://webhook.discord.com/hogehoge")
+		require.Equal(t, nDB.SlackWebhook, "")
+	})
+
+	t.Run("all changes", func(t *testing.T) {
+		ctx := context.Background()
+
+		tool, err := test.NewTestTool()
+		require.NoError(t, err)
+		defer tool.Close()
+
+		err = tool.ClearNotice(ctx)
+		require.NoError(t, err)
+
+		h, err := tool.Handler()
+		require.NoError(t, err)
+
+		u, err := tool.NewUser(ctx)
+		require.NoError(t, err)
+
+		n, err := u.CreateNotice()
+		require.NoError(t, err)
+		_, err = n.CreateDB(ctx, tool.DB)
+		require.NoError(t, err)
+
+		form := contents.NewMultipart()
+		form.Insert("discord_webhook", "https://webhook.discord.com/hogehoge")
+		form.Insert("slack_webhook", "https://webhook.slack.com/aaaaaaa")
+		form.Insert("mail", "test@cateiru.com")
+
+		m, err := mock.NewFormData("/", form, http.MethodPut)
+		require.NoError(t, err)
+
+		err = u.HandlerSession(ctx, tool.DB, m)
+		require.NoError(t, err)
+
+		e := m.Echo()
+
+		err = h.UpdateNoticeHandler(e)
+		require.NoError(t, err)
+
+		nDB, err := tool.DB.Client.Notice.
+			Query().
+			Where(notice.UserID(u.User.ID)).
+			First(ctx)
+		require.NoError(t, err)
+
+		require.Equal(t, nDB.Mail, "test@cateiru.com")
+		require.Equal(t, nDB.DiscordWebhook, "https://webhook.discord.com/hogehoge")
+		require.Equal(t, nDB.SlackWebhook, "https://webhook.slack.com/aaaaaaa")
+	})
+
 	test.LoginTestGet(t, func(h *handler.Handler, e echo.Context) error {
 		return h.UpdateNoticeHandler(e)
-	})
-}
-
-func TestDeleteNoticeHandler(t *testing.T) {
-	test.Init()
-
-	test.LoginTestGet(t, func(h *handler.Handler, e echo.Context) error {
-		return h.DeleteNoticeHandler(e)
 	})
 }
