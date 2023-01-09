e21d5a9c00d65ae0550206b8e797263d76705328
Yuto Watanabe <yuto.w51942@gmail.com>
2023-01-09 15:25:49 +0900

    イケイケにした

---
+++ @types/vanta.d.ts
@@ -0,0 +1,20 @@
+declare module 'vanta/dist/vanta.fog.min' {
+  export default function set(c: Config): VantaEffect;
+}
+
+export interface Config {
+  el: HTMLElement;
+  mouseControls?: boolean;
+  touchControls?: boolean;
+  gyroControls?: boolean;
+  minHeight?: number;
+  minWidth?: number;
+  highlightColor?: number;
+  midtoneColor?: number;
+  lowlightColor?: number;
+  baseColor?: number;
+}
+
+export interface VantaEffect {
+  destroy(): void;
+}
--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,12 +1,53 @@
-import {Center, Avatar} from '@chakra-ui/react';
+import {Center, Avatar, useColorMode} from '@chakra-ui/react';
 import React from 'react';
+import FOG from 'vanta/dist/vanta.fog.min';
+import type {VantaEffect} from '../../@types/vanta';
 import {Public} from '../../utils/types';
 import {Section} from '../Common/Section';
 import ProfileText from './ProfileText';
 
 const Profile: React.FC<{next: () => void; data: Public}> = ({next, data}) => {
+  const {colorMode} = useColorMode();
+  const [vantaEffect, setVantaEffect] = React.useState<VantaEffect | null>(
+    null
+  );
+  const myRef = React.useRef<HTMLDivElement>(null!);
+
+  React.useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        FOG({
+          el: myRef.current,
+          baseColor: colorMode === 'dark' ? 0x1a202c : 0xffffff,
+          highlightColor: 0xcf2ba1,
+          midtoneColor: 0x2bc4cf,
+          lowlightColor: 0x572bcf,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (!vantaEffect) return;
+
+    vantaEffect.destroy();
+
+    setVantaEffect(
+      FOG({
+        el: myRef.current,
+        baseColor: colorMode === 'dark' ? 0x1a202c : 0xffffff,
+        highlightColor: 0xcf2ba1,
+        midtoneColor: 0x2bc4cf,
+        lowlightColor: 0x572bcf,
+      })
+    );
+  }, [colorMode]);
+
   return (
-    <Section next={next}>
+    <Section next={next} r={myRef}>
       <Center mb="1rem">
         <Avatar src={data.avatar_url} size="lg" />
       </Center>
