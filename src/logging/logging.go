1c6cbe1613d33e35a1033f2bb624d4ad468db708
Yuto Watanabe <yuto.w51942@gmail.com>
2023-01-30 13:32:47 +0900

    prod時のログをcloud loggingに対応

--- src/logging/logging.go
+++ src/logging/logging.go
@@ -8,6 +8,33 @@ import (
 var Logger *zap.Logger = nil
 var Sugar *zap.SugaredLogger = nil
 
+// Cloud Logging LOG Level Severity
+var logLevelSeverity = map[zapcore.Level]string{
+	zapcore.DebugLevel:  "DEBUG",
+	zapcore.InfoLevel:   "INFO",
+	zapcore.WarnLevel:   "WARNING",
+	zapcore.ErrorLevel:  "ERROR",
+	zapcore.DPanicLevel: "CRITICAL",
+	zapcore.PanicLevel:  "ALERT",
+	zapcore.FatalLevel:  "EMERGENCY",
+}
+
+func EncodeLevel(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(logLevelSeverity[l])
+}
+
+func newProductionEncoderConfig() zapcore.EncoderConfig {
+	cfg := zap.NewProductionEncoderConfig()
+
+	cfg.TimeKey = "time"
+	cfg.LevelKey = "severity"
+	cfg.MessageKey = "message"
+	cfg.EncodeLevel = EncodeLevel
+	cfg.EncodeTime = zapcore.RFC3339NanoTimeEncoder
+
+	return cfg
+}
+
 func InitLogging(mode string) {
 	var logConfig zap.Config
 
@@ -20,7 +47,8 @@ func InitLogging(mode string) {
 		logConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	case "prod":
 		logConfig = zap.NewProductionConfig()
-		logConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+		logConfig.Level.SetLevel(zap.ErrorLevel)
+		logConfig.EncoderConfig = newProductionEncoderConfig()
 	default:
 		return
 	}
