83b245775a26cc48df3b83899fbca87504931f98
Yuto Watanabe <yuto.w51942@gmail.com>
2023-03-22 00:15:44 +0900

    404ページを可愛くした

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -1,4 +1,4 @@
-import {Center, Heading} from '@chakra-ui/react';
+import {Box, Center, Heading, Text} from '@chakra-ui/react';
 import useLanguage from './useLanguage';
 
 export const NotFound = () => {
@@ -6,16 +6,31 @@ export const NotFound = () => {
 
   return (
     <Center h="100vh">
-      <Heading
+      <Box
         backgroundImage="linear-gradient(135deg, #2bc4cf, #572bcf, #cf2ba1)"
         backgroundClip="text"
-        textAlign="center"
       >
-        {convertLang({
-          ja: '404 | お探しのページは見つかりません',
-          en: '404 | Not Found',
-        })}
-      </Heading>
+        <Center mb="1rem">
+          <Text fontSize="1.5rem" fontWeight="bold">
+            ﾊﾞﾝﾊﾞﾝﾊﾞﾝﾊﾞﾝﾊﾞﾝﾊﾞﾝﾊﾞﾝﾞﾝﾊﾞﾝ
+            <br />
+            ﾊﾞﾝ&nbsp;&nbsp;&nbsp;∧＿∧&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ﾊﾞﾝﾊﾞﾝﾊﾞﾝ
+            <br />
+            ﾊﾞﾝ&nbsp;&nbsp;(∩`･ω･)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ﾊﾞﾝﾊﾞﾝ
+            <br />
+            &nbsp;&nbsp;&nbsp;＿/_ﾐつ/￣￣￣/
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;＼/＿＿＿/￣￣
+            <br />
+          </Text>
+        </Center>
+        <Heading textAlign="center">
+          {convertLang({
+            ja: '404 | お探しのページは見つかりません',
+            en: '404 | Not Found',
+          })}
+        </Heading>
+      </Box>
     </Center>
   );
 };
