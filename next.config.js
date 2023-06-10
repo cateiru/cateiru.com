6d80074a85074e39095161b866063b8025860ce6
Yuto Watanabe <yuto.w51942@gmail.com>
2023-06-11 02:02:24 +0900

    スクロール位置保持する

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withInterceptStdout = require('next-intercept-stdout');
 module.exports = withInterceptStdout(
   {
     reactStrictMode: true,
+    experimental: {
+        scrollRestoration: true,
+    },
   },
   text => (text.includes('Duplicate atom key') ? '' : text)
 );
