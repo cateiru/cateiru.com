16e6827ed37bff6bb70bf240e39b957b9dcab402
cateiru <git@cateiru.com>
2026-04-10 18:21:16 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Logo } from "@/components/Logo";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>cateiru.com</h1>
+      <div>
+      <Logo />
+      </div>
+       <h1 className={styles.title}>cateiru.com</h1>
     </main>
   );
 }
